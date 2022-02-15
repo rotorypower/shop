7e98db8db8468eda74d6928ff01bb9c3915f1ac5
Kavalevski Yuri <kavalevskiyuri@gmail.com>
2022-02-16 00:43:18 +0300

    Add classwork project shop

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'shop',
+    'tool',
 ]
 
 MIDDLEWARE = [
@@ -69,18 +73,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shop.wsgi.application'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "shop",
-        "USER": "shop",
-        "PASSWORD": "shop",
-        "HOST": "localhost",
-        "PORT": 5432,
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'shop',
+        'PASSWORD': 'shop',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -119,7 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = "/static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- shop/tool/admin.py
+++ shop/tool/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from tool.models import Product, Purchase
+
+
+class PurchaseInline(admin.TabularInline):
+    model = Purchase
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'external_id', 'cost', 'status']
+    search_fields = ['title']
+    inlines = [
+       PurchaseInline,
+    ]
+
+    list_filter = ['status']
